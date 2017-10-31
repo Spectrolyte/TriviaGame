d08a9eda89b1ec095e135216bc4acf5dc8476417
Diana <dianaltecson@gmail.com>
2017-10-31 10:51:13 -0700

    functioning score count

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,70 @@
 // variable declaration
-var correct;
-var wrong;
+var correct =0 ;
+var wrong = 0;
 var questionAnswer = {
 	'What is the color of the sky?': ['blue','green','red','purple'],
 	'What kind of bonds does water have?':['covalent','hydrogen','ionic','oxygen'],
-	'Which is NOT a primary color?':['blue','yellow','green','red'],
+	'Which is NOT a primary color?':['bloo','yellow','purple','red'],
 	'Which Hogwarts house did Harry Potter belong to?':['ravenclaw','hufflepuff','slytherin','gryffindor'],
 	'Which GoT house has the sigil of a Lion?':['Stark','Lannister','Baratheon','Targeryeon']
 }
 var currentQuestion;
-var answer;
+var answerKey = ['blue','hydrogen','green','gryffindor','Lannister'];
 var asked = [];
+var userAnswer;
 
 $(document).ready(function () {
+
+// consider putting function in an object
 
 function generateRandomNum (min, max) {
 	return Math.floor(Math.random() * max ) - min;
 }
 
 // select question from QA obj
+// run this function after x amount of seconds
 function displayQuestion () {
 	// select question only if it hasn't already been selected, then push to array of asked questions
-	// randomize which question is displayed
+	// randomize which question is displayed -- do this later
 	for (var key in questionAnswer) {
 		if (!asked.includes(key)) {
 			asked.push(key);
 			$('#current-question').text(key);
+			// select button, set value attr equal to answer
+			// display answer options
+			// randomize answer options to buttons -- do this later
 			for (var i=0; i < questionAnswer[key].length; i++) {
-				$('.option' + [i] + ' span').text(questionAnswer[key][i]);
+				$('.option' + [i] + ' span').attr('value',questionAnswer[key][i]).text(questionAnswer[key][i]);
 			}
 			return;
 		}
-	}
-
-	// randomize answer options to buttons
-		// select button, set value attr equal to answer
+	}	
 }
 
-$('.test').on('click', displayQuestion);
-
 // scoring conditions
+// check user answer with answer bank
+function checkAnswer () {
+	// if selected correct answer, increment correct score by one
+	if (answerKey.includes(userAnswer)) {
+		correct++;
+		alert('correct');
+	} else {
+		wrong++;
+		alert('wrong');
+	}
+}
+
+displayQuestion();
+
+// upon button click, store selected answer into user answer and cross-check with answer key
+$('.options span').on('click', function () {
+	userAnswer = $(this).attr('value');
+	checkAnswer();
+	// display correct answer
+	// dispaly next question
+		// at the last correct answr reveal, display score and ask player to play again WITHOUT refreshing the page
+})
+
+$('.test').on('click', displayQuestion);
 
 });
