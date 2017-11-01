17800149813020372ac3c42c01b3a99f2a9eccba
Diana <dianaltecson@gmail.com>
2017-11-01 15:33:02 -0700

    updated questions and craeted hide/show functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,33 +7,58 @@ var correctAnswer;
 // array of question-answer objects with question, possible answers, and the correct answer
 var questionAnswer = [
 
-	{question: 'What is the color of the sky?',
-	answers: ['blue','green','red','purple'],
-	correct: 'blue',
-	asked: false},
+	{question: 'Which character was Muggle-born?',
+	answers: ['Harry Potter','Hermoine Granger','Ron Weasley','Luna Lovegood'],
+	correct: 'Hermoine Granger',
+	asked: false
+	image: /*insert url here*/},
 	
-	{question: 'What kind of bonds does water have?',
-	answers: ['covalent','hydrogen','ionic','oxygen'],
-	correct: 'hydrogen',
-	asked: false},
+	{question: 'What is the real name of Voldemort?',
+	answers: ['Albus Dumbledore','Morte Donovan','Salazar Serpient','Tom Riddle'],
+	correct: 'Tom Riddle',
+	asked: false
+	image: /*insert url here*/},
 
-	{question: 'Which is NOT a primary color?',
-	answers: ['blue','yellow','purple','red'],
-	correct: 'purple',
-	asked: false},
+	{question: 'What was the name of the three-headed dog at guarded the Philosopher\'s Stone?',
+	answers: ['Fluffy','Benjamin','Cerberus','Olly'],
+	correct: 'Fluffy',
+	asked: false
+	image: /*insert url here*/},
 
-	{question: 'Which Hogwarts house did Harry Potter belong to?',
-	answers: ['ravenclaw','hufflepuff','slytherin','gryffindor'],
-	correct: 'gryffindor',
-	asked: false},
+	{question: 'Which Hogwarts house has the sigil of a serpent?',
+	answers: ['Ravenclaw','Hufflepuff','Slytherin','Gryffindor'],
+	correct: 'Slytherin',
+	asked: false
+	image: /*insert url here*/},
 
-	{question: 'Which GoT house has the sigil of a Lion?',
-	answers: ['Stark','Lannister','Baratheon','Targeryeon'],
-	correct: 'Lannister',
-	asked: false}
+	{question: 'What kind of Patronus protected Harry Potter against the dementors?',
+	answers: ['Eagle','Doe','Lion','Dragon'],
+	correct: 'Doe',
+	asked: false
+	image: /*insert url here*/}
 ];
 
 $(document).ready(function () {
+	// click events
+		// click start game button to initiate game
+		$('.start').click(displayQuestionAnswers);
+		// capture user-answer upon clicking a button
+		$('.options').click(checkAnswer);
+
+	function hide (section) {
+		$(section).hide();
+	}
+
+	function show (section) {
+		$(section).show();
+	}
+
+	// hide question, timer, and answer buttons upon page load
+	// show question, timer, and answer buttons after start button is clicked. hide start button
+	// after answering a question or timer runs out,
+		// hide question, timer, and answer buttons
+		// display if user got it right or wrong, the correct answer, the button the user clicked, and an image
+
 	// generate random number between min and max, inclusive
 	function getRandomNum (min, max) {
 		return Math.floor(Math.random() * max) - min;
@@ -69,24 +94,33 @@ $(document).ready(function () {
 		}
 	}
 
-	// click start game button to initiate game
-	$('.start').click(displayQuestionAnswers);
-
-	// capture user-answer upon clicking a button
-	$('.options').click(checkAnswer);
+	// display the correct answer
+	function displayCorrectAnswer () {
+		// fade out buttons that are not the user's answer
+		for (var i=0; i < 4; i++) {
+			if ($('.option' + i).attr('value') !== userAnswer) {
+				hide($('.option' + i));
+			}
+		}
+		// fade out time remaining
+		hide($('.time'));
+		// fade out question
+		// display picture or gif along with the correct answer
+	}
 
 	// check user-answer against the correct answer
 	function checkAnswer () {
 		userAnswer = $(this).attr('value');
+
 		if (userAnswer === correctAnswer) {
 			correct++;
-			console.log('right');
+			$('.right-wrong').text('right');
 			// display 'right' and correct answer page
 			// display next question
 		}
 		else {
 			wrong++;
-			console.log('wrong');
+			$('.right-wrong').text('wrong');
 			// display 'wrong' and correct answer page
 			// display next question
 		}
